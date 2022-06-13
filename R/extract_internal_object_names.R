a4899c56c626b9af3cbd8970978cb8cc9840e078
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-06-13 21:10:30 +0200

    Update extract_internal_object_names.R

--- R/extract_internal_object_names.R
+++ R/extract_internal_object_names.R
@@ -15,9 +15,13 @@ extract_internal_object_names <- function() {
   internal_objects <- purrr::discard(internal_objects, ~startsWith(.x, prefix = "."))
 
   # get a list of all S3 methods, which won't be included in `getNamespaceExports()`
-  s3_method_names <- rlang::env_names(eval(rlang::call2(":::", package_name, expr(.__S3MethodsTable__. ))))
+  s3_method_names <- env_names(eval(call2(":::", package_name, expr(.__S3MethodsTable__. ))))
 
+  # exclude these methods
   internal_objects <- setdiff(internal_objects, s3_method_names)
+
+  # there might still be S3 methods left if they belong to generics defined in base package
+  purrr::discard(ls, ~grepl(x = .x, pattern = "(\\w+)(\\.)(\\w+)"))
 
   return(internal_objects)
 }
