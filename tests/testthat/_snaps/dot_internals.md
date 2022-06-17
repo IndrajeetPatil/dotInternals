53a1449f61bda53c6b4b7ce47d49eda5fe8f09b4
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-06-17 18:57:14 +0200

    more tests

--- tests/testthat/_snaps/dot_internals.md
+++ tests/testthat/_snaps/dot_internals.md
@@ -16,3 +16,32 @@
     Output
       [1] "if1" "if2"
 
+# it changes function names
+
+    Code
+      dotInternals::dot_internals()
+    Message <rlang_message>
+      Prefixing names of 2 internal functions with a `.`
+    Message <cliMessage>
+      * `if1()`
+      * `if2()`
+      --------------------------------------------------------------------------------
+      v Internal function names successfully changed.
+      ! Please review the changes carefully!
+      i Re-document with `roxygen2::roxygenise()`.
+      --------------------------------------------------------------------------------
+
+---
+
+    Code
+      dotInternals:::.extract_internal_function_names()
+    Output
+      character(0)
+
+---
+
+    Code
+      dotInternals::dot_internals()
+    Message <cliMessage>
+      i There are no internal functions whose names need to be changed.
+
--- tests/testthat/test-dot_internals.R
+++ tests/testthat/test-dot_internals.R
@@ -11,6 +11,7 @@ test_that("it produces expected message when no internal functions are found", {
 })
 
 build_file <- paste0(rprojroot::find_testthat_root_file(), "/assets/testpackage1_1.0.0.tar.gz")
+temp_dir <- paste0(rprojroot::find_testthat_root_file(), "/assets/pkg-with-internals-temp")
 
 test_that("it changes only internal *function* names without `.` prefix", {
   pkg <- test_path("assets/pkg-with-internals")
@@ -27,4 +28,28 @@ test_that("it changes only internal *function* names without `.` prefix", {
 test_that("check clean up", {
   expect_false("testpackage1" %in% loadedNamespaces())
   expect_false(file.exists(build_file))
+})
+
+
+test_that("it changes function names", {
+  pkg <- test_path("assets/pkg-with-internals")
+  pkg_temp <- test_path("assets/pkg-with-internals-temp")
+  fs::dir_copy(pkg, pkg_temp, overwrite = TRUE)
+
+  withr::with_dir(pkg_temp, {
+    # first time, the package runs, it should find and rename internal functions
+    pkgbuild::build(quiet = TRUE)
+    expect_snapshot(dotInternals::dot_internals())
+
+    # loading the package with changed files shouldn't find any functions to dot
+    pkgload::load_all(quiet = TRUE)
+    expect_snapshot(dotInternals:::.extract_internal_function_names())
+
+    # moreover, if we were to run the function again, it shouldn't do anything
+    expect_snapshot(dotInternals::dot_internals())
+
+    pkgload::unload(quiet = TRUE)
+  })
+
+  unlink(temp_dir, recursive = TRUE, force = TRUE)
 })
